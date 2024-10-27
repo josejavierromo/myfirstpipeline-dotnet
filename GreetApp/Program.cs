7698d53325441a6a4411401897d1d437269036c5
Jose Javier <jose.romoe@gmail.com>
2024-10-27 18:41:26 +0100

    feat: Implements GreetApp.

---
+++ GreetApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using GreetBot;
+
+Greeting greeting = new Greeting();
+ConsoleKeyInfo consoleKeyInfo;
+
+do
+{
+    string message = greeting.Greet();
+
+    Console.WriteLine(message);
+
+    Console.WriteLine("Press any key to get next greet message, ESC to exit");
+
+    consoleKeyInfo = Console.ReadKey();
+}
+while (consoleKeyInfo.Key != ConsoleKey.Escape);
